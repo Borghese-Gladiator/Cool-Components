64f65e9c154010d2a2e571e140d9587ca9d6f56f
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-20 04:32:22 -0500

    Create credit_card.js

---
+++ src/pages/credit_card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GlassCreditCard from '../components/GlassCreditCard';
+
+const CreditCardPage = () => {
+    return (
+        <GlassCreditCard />
+    )
+};
+
+export default CreditCardPage;
