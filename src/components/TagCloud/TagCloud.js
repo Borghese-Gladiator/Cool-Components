1045b21b6700e8049c1dc7924f56e070432b4358
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-20 19:02:00 -0500

    fix warnings (add link to blank href)

--- src/components/TagCloud/TagCloud.js
+++ src/components/TagCloud/TagCloud.js
@@ -4,21 +4,21 @@ import './TagCloud.css';
 function TagCloud() {
     return (
         <ul class="cloud" role="navigation" aria-label="Webdev word cloud">
-            <li><a href="#" data-weight="4">HTTP</a></li>
-            <li><a href="#" data-weight="1">Ember</a></li>
-            <li><a href="#" data-weight="5">Sass</a></li>
-            <li><a href="#" data-weight="8">HTML</a></li>
-            <li><a href="#" data-weight="6">FlexBox</a></li>
-            <li><a href="#" data-weight="4">API</a></li>
-            <li><a href="#" data-weight="5">VueJS</a></li>
-            <li><a href="#" data-weight="6">Grid</a></li>
-            <li><a href="#" data-weight="2">Rest</a></li>
-            <li><a href="#" data-weight="9">JavaScript</a></li>
-            <li><a href="#" data-weight="3">Animation</a></li>
-            <li><a href="#" data-weight="7">React</a></li>
-            <li><a href="#" data-weight="8">CSS</a></li>
-            <li><a href="#" data-weight="1">Cache</a></li>
-            <li><a href="#" data-weight="3">Less</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="4">HTTP</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="1">Ember</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="5">Sass</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="8">HTML</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="6">FlexBox</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="4">API</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="5">VueJS</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="6">Grid</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="2">Rest</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="9">JavaScript</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="3">Animation</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="7">React</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="8">CSS</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="1">Cache</a></li>
+            <li><a href="https://dev.to/alvaromontoro/create-a-tag-cloud-with-html-and-css-1e90" data-weight="3">Less</a></li>
         </ul>
     )
 }
